11976d18b965be7f3c67c704d314b2b0dd5e625c
vxh <vuxuanhoa2403@gmail.com>
2024-10-08 10:27:58 +0700

    profile prodetails

--- fe/src/Routes.js
+++ fe/src/Routes.js
@@ -5,6 +5,8 @@ import ProductsPage from "../src/pages/ProductsPage.jsx";
 import BestSellinngPage from "../src/pages/BestSellinngPage.jsx";
 import EventsPgae from "../src/pages/EventsPgae.jsx";
 import FaqPage from "../src/pages/FaqPgae.jsx";
+import ProductDetailsPage from "../src/pages/ProductDetailsPage.jsx";
+import ProfilePage from "../src/pages/ProfilePage.jsx";
 export {
   LoginPage,
   SignupPage,
@@ -13,4 +15,6 @@ export {
   BestSellinngPage,
   EventsPgae,
   FaqPage,
+  ProductDetailsPage,
+  ProfilePage,
 };
