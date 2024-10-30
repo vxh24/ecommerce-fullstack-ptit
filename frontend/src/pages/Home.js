708db323828905e09998e1964f9878936e96446d
vxh <vuxuanhoa2403@gmail.com>
2024-10-31 03:02:22 +0700

    header footer

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,214 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import Marquee from "react-fast-marquee";
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className="home-wraper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative">
+                <img src="/images/main-banner-1.jpg" className="img-fluid rounded-3" alt="main banner" />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From $999.0 or $41.0</p>
+                  <Link className='button'>BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="div d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
+                  <img src="/images/catbanner-01.jpg" className="img-fluid rounded-3" alt="main banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>From $999.0 <br />or $41.0</p>
+
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img src="/images/catbanner-02.jpg" className="img-fluid rounded-3" alt="main banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>From $999.0 <br /> or $41.0</p>
+
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img src="/images/catbanner-03.jpg" className="img-fluid rounded-3" alt="main banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>From $999.0 <br /> or $41.0</p>
+
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img src="/images/catbanner-04.jpg" className="img-fluid rounded-3" alt="main banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>From $999.0 <br /> or $41.0</p>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="servies d-flex align-items-center justify-content-between">
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service.png" alt="" />
+                  <div>
+                    <h6>Free Shiping</h6>
+                    <p className='mb-0'>From all order over $100</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service-02.png" alt="" />
+                  <div>
+                    <h6>Daily Surprise Offer</h6>
+                    <p className='mb-0'>Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service-03.png" alt="" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className='mb-0'>Shop with an expert</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service-04.png" alt="" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className='mb-0'>Get factory direct price</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service-05.png" alt="" />
+                  <div>
+                    <h6>Secure Payment</h6>
+                    <p className='mb-0'>100% Product Payment</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="categories d-flex align-items-center justify-content-between flex-wrap">
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Cameras</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Smart TV</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Smart Watch</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Cameras</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Cameras</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Smart TV</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Smart Watch</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap align-items-center'>
+                  <div>
+                    <h6>Cameras</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="marque-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="marquee-inner-wrapper card-wrapper">
+                <Marquee className='d-flex'>
+                  <div className='mx-4 w-25'>
+                    <img src="images/brand-01.png" alt="brand" />
+                  </div>
+                  <div className='mx-4 w-25'>
+                    <img src="images/brand-02.png" alt="brand" />
+                  </div>
+                  <div className='mx-4 w-25'>
+                    <img src="images/brand-03.png" alt="brand" />
+                  </div>
+                  <div className='mx-4 w-25'>
+                    <img src="images/brand-04.png" alt="brand" />
+                  </div>
+                  <div className='mx-4 w-25'>
+                    <img src="images/brand-05.png" alt="brand" />
+                  </div>
+                  <div className='mx-4 w-25'>
+                    <img src="images/brand-06.png" alt="brand" />
+                  </div>
+                  <div className='mx-4 w-25'>
+                    <img src="images/brand-07.png" alt="brand" />
+                  </div>
+                  <div className='mx-4 w-25'>
+                    <img src="images/brand-08.png" alt="brand" />
+                  </div>
+                </Marquee>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
